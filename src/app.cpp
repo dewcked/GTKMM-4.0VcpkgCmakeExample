96fe8f100e3be03faa713d2e03fc644ac9ad367d
dewcked <48849740+dewcked@users.noreply.github.com>
2022-03-30 16:54:39 +0900

    Fix indent

--- src/app.cpp
+++ src/app.cpp
@@ -6,18 +6,18 @@
 class MyWindow : public Gtk::Window
 {
 public:
-	MyWindow();
+  MyWindow();
 };
 
 MyWindow::MyWindow()
 {
-	set_title("Basic application");
-	set_default_size(200, 200);
+  set_title("Basic application");
+  set_default_size(200, 200);
 }
 
 int main(int argc, char* argv[])
 {
-	auto app = Gtk::Application::create("org.gtkmm.examples.base");
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
-	return app->make_window_and_run<MyWindow>(argc, argv);
+  return app->make_window_and_run<MyWindow>(argc, argv);
 }
